0aef431fa481de2d909fa619a942950aa370cbde
naaaaya <naaya981024@gmail.com>
2018-08-23 22:01:10 +0900

    本を登録して一覧表示できるようにした

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,21 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, except: :index
   def index
     @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(create_params)
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def create_params
+    params.require(:book).permit(:title, :author, :start_date, :end_date).merge(user_id: current_user)
   end
 end
