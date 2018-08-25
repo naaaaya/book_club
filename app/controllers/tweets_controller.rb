f60b24d43b18c43de30952555e630478e9031b46
naaaaya <naaya981024@gmail.com>
2018-08-25 21:38:39 +0900

    ツイートを投稿できるようにした

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,21 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @book = Book.find(params[:book_id])
+    @tweets = @book.tweets.order("created_at DESC").limit(20)
+    @tweet = @book.tweets.new
+  end
+
+  def create
+    @book = Book.find(params[:book_id])
+    @tweet = @book.tweets.create(create_params)
+    redirect_to book_tweets_path
+  end
+
+  private
+
+  def create_params
+    params.require(:tweet).permit(:content).merge(user_id: current_user.id)
+  end
 end
